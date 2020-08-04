4363ccd2e3d023b409cce63dd9ef97624a89cff6
Andrew Pishchulin <ap@elinsoftware.com>
2020-08-04 14:13:40 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.css";
+import App from './app'
+import {FocusStyleManager} from '@blueprintjs/core'
+
+var div = document.createElement('div');
+div.setAttribute("id", "buzzPost-root");
+document.body.appendChild(div);
+
+// update these props to test in DEV mode locally
+if (process.env.NODE_ENV === 'development') {
+    window.buzzPostBroker = "https://hub11.dev-labs.io"
+    window.NOW = {
+        "user":{
+            "userID":"4154a8a9db313200cc38f7fdbf961907",
+            "name":"Alex Moreno"
+        }
+    }
+    window.prefixID = "ven01994.service-now.com"
+}
+
+if (window.NOW.user.name!=undefined && window.NOW.user.name!="guest") {
+    if (window.buzzPostBroker!=undefined && window.buzzPostBroker!='') {
+        FocusStyleManager.onlyShowFocusOnTabs()
+        ReactDOM.render(<App />, document.getElementById("buzzPost-root"))
+    } else {
+        console.log('%cbuzzPost: message broker server is not defined. Please provide message broker url in window.buzzPostBroker','color:blue')
+    }
+}
